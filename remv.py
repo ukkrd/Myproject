26aa95c19459befe1a4f1be1be1ada7c05344603
Ujjwal Kumar <113245166+ukkrd@users.noreply.github.com>
2022-11-07 20:18:05 +0530

    Add files via upload
    1.Find all the possible combinations of char within the string. For example “123”---> “132” , “213”, “231” , “312” ……

---
+++ remv.py
@@ -0,0 +1,16 @@
+#2.Write a method which will remove any given character from a String. Example remove
+def remv(m,n):
+    m=m.replace(n,"")
+    print(m)
+    return 
+        
+stri=str(input("Enter a string:"))
+char=str(input("Enter a character to remove:"))
+remv(stri,char)
+        
+   
+
+
+
+    
+    
